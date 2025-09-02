4be4a285b16bc230c38d41e8b7c684353fd17d76
Farshad Habibzade <farshadhz02@gmail.com>
2025-09-02 21:16:57 +0330

    9/2/25 - 9:16 PM

--- FD.TaskManager.API/Program.cs
+++ FD.TaskManager.API/Program.cs
@@ -11,10 +11,7 @@ using TaskManager.Logic.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<HttpClient>();
 builder.Services.AddSingleton<DataBaseConnection>();
